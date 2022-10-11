8d5ad372201158909001020fc28b796387c8c418
ZhenLII <svjiang@foxmail.com>
2022-10-11 10:06:14 +0800

    添加符号常量

---
+++ src/main/java/constants/SymbolConstants.java
@@ -0,0 +1,45 @@
+package constants;
+
+/**
+ * @author JiangSenwei
+ */
+public class SymbolConstants {
+    public static final String DOT = ".";
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
+    public static final String SEMICOLON = ";";
+    public static final String SLASH = "/";
+    public static final String BACKSLASH = "\\";
+
+    public static final String HYPHEN = "-";
+    public static final String DASH = HYPHEN;
+
+    public static final String UNDERSCORE = "_";
+    public static final String UNDERLINE = UNDERSCORE;
+
+    public static final String TILDE = "~";
+    public static final String BACKQUOTE= "`"; // 反引号
+    public static final String EXCLAMATION= "!";
+    public static final String AT = "@";
+    public static final String SHARP = "#";
+    public static final String DOLLAR = "$";
+
+    public static final String MOD = "%";
+    public static final String PERCENT = MOD;
+
+    public static final String CIRCUMFLEX = "^"; // 扬抑符
+
+    public static final String AND = "&";
+
+    public static final String ASTERISK = "*";
+    public static final String STAR = ASTERISK;
+
+    public static final String PIPE = "|"; // 管道符，OR
+    public static final String OR = PIPE;
+
+    public static final String QUESTION = "?";
+
+    public static final String SPACE = " ";
+
+    public static final String EQUAL = "=";
+}
